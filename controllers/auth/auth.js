3c798219526872da4d71645ffd1e4963e78d3c62
Ausaf1 <ausaf.esrar@gmail.com>
2023-03-03 20:22:55 +0530

    auth setup

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -15,6 +15,19 @@ const register = catchAsyncErrors(async (req, res, next) => {
   }
 });
 
+const registerProfile = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const data = await auth.registerUserProfile(req.body);
+    res.status(200).json({
+      status: true,
+      message: "Profile registration successful",
+      data,
+    });
+  } catch (e) {
+    next(createError(e.statusCode, e.message));
+  }
+});
+
 const login = catchAsyncErrors(async (req, res, next) => {
   try {
     const data = await auth.loginUser(req.body);
@@ -24,6 +37,21 @@ const login = catchAsyncErrors(async (req, res, next) => {
       data,
     });
   } catch (e) {
+    next(createError(e.statusCode, e.message));
+  }
+});
+
+const getUserProfile = catchAsyncErrors(async (req, res, next) => {
+  try {
+    console.log(req.params.id);
+    const data = await auth.getUserProfile(req.params.id);
+    res.status(200).json({
+      status: true,
+      message: "User profile",
+      data,
+    });
+  } catch (e) {
+    console.log(e);
     next(createError(e.statusCode, e.message));
   }
 });
@@ -45,4 +73,6 @@ module.exports = {
   register,
   login,
   getAllUsers,
+  registerProfile,
+  getUserProfile,
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,22 @@
 const router = require("express").Router();
 const user = require("../controllers/auth/auth");
 const auth = require("../middlewares/auth");
-const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 // @desc    Register a user
 // @route   POST /api/auth/register
 router.post("/register", user.register);
 
+// @desc    Register profile of a user
+// @route   POST /api/auth/register/profile
+router.post("/register/profile", user.registerProfile);
+
 // @desc    Login a user
 // @route   POST /api/auth/login
 router.post("/login", user.login);
+
+// @desc    Get user profile
+// @route   GET /api/auth/profile
+router.get("/profile/:id", user.getUserProfile);
 
 // @desc    Get user all
 // @route   GET /api/auth/all
